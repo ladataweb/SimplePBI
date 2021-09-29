166a32a34e8b7220367e9a088abd0bd1c1203c1f
Ignacio Barrau <ibarrau@users.noreply.github.com>
2021-09-29 10:19:04 -0300

    Creating token class
    Creating token class to handle a unique place for authentication. The other classes can be created with a single parameter "token".

--- simplepbi/admin/__init__.py
+++ simplepbi/admin/__init__.py
@@ -1,86 +1,24 @@
 import json
 import requests
+import token
 
-
-class admin():
+class Admin():
     """Simple library to use the Power BI api and obtain datasets from it.
     """
 
-    def __init__(self, tenant_id=None, power_bi_client_id=None, power_bi_username=None, power_bi_password=None, power_bi_secret=None, use_service_principal=False):
-        """Create a simplePBI object to get authentication token.
-        
-        You can use:
-            1) User and pass authentication
-                Provide:    tenant_id
-                            power_bi_client_id
-                            power_bi_username
-                            power_bi_password
-            2) Service principal authentication (set use_service_principal to True)
-                Provide:    tenant_id
-                            power_bi_client_id
-                            power_bi_secret
-                            use_service_principal=True
+    def __init__(self, token):
+        """Create a simplePBI object to request admin API
         Args:
-            tenant_id : String
-                Tenant ID to connect to.
-            power_bi_client_id : String
-                Client ID (also known as App ID)
-            power_bi_username : String
-                Username to use when authenticating.
-            power_bi_password : String
-                Password to use when authenticating.
-            power_bi_secret : String
-                The secret to authenticate with the Client ID.
-            use_service_principal : Bool
-                False by default, if set to true it will authenticate using power_bi_client_id and power_bi_secret.
+            token: String
+                Bearer Token to use the Power Bi Rest API
         """
-
-        POWER_BI_RESOURCE_ENDPOINT = "https://analysis.windows.net/powerbi/api"
-        MICROSOFT_OAUTH2_API_ENDPOINT = "https://login.microsoftonline.com/" + tenant_id + "/oauth2/token/"
-        if not use_service_principal:
-            try:
-                url = MICROSOFT_OAUTH2_API_ENDPOINT
-                body = {
-                    "resource":POWER_BI_RESOURCE_ENDPOINT, 
-                    "client_id":power_bi_client_id,
-                    "grant_type":"password",
-                    "username":power_bi_username,
-                    "password":power_bi_password,
-                    "scope":"openid"                    
-                }
-                headers = {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                    }
-                r = requests.post(url = url, data = body, headers = headers)
-                access_token = r.json().get('access_token')
-                self.token = access_token
-            except requests.exceptions.HTTPError as ex:
-                print(ex)
-            except Exception as e:
-                print(e)
-        else:
-            try:
-                url = MICROSOFT_OAUTH2_API_ENDPOINT #is this different?
-                body = {
-                    "grant_type":"client_credentials",
-                    "client_id":power_bi_client_id,
-                    "client_secret":power_bi_secret,
-                    "resource":POWER_BI_RESOURCE_ENDPOINT            
-                }
-                headers = {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                    }
-                r = requests.post(url = url, data = body, headers = headers)
-                access_token = r.json().get('access_token')
-                self.token = access_token
-            except requests.exceptions.HTTPError as ex:
-                print(ex)
+        self.token = token
     
-    def get_datasets(auth_token):
+    def get_datasets(self):
         """Returns a list of datasets for the organization..
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         ### Returns
         ----
@@ -89,18 +27,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/datasets"
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_datasets_in_group(auth_token, workspace_id):
+    def get_datasets_in_group(self, workspace_id):
         """Returns a list of datasets from the specified workspace.
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         workspace_id:
             The Power Bi workspace id. You can take it from PBI Service URL
@@ -111,18 +49,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/groups/{}/datasets".format(workspace_id)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_dataset_users(auth_token, dataset_id):
+    def get_dataset_users(self, dataset_id):
         """Returns a list of users that have access to the specified dataset (Preview).
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         dataset_id:
             The Power Bi Dataset id. You can take it from PBI Service URL
@@ -133,18 +71,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/datasets/{}/users".format(dataset)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_datasources(auth_token, dataset_id):
+    def get_datasources(self, dataset_id):
         """Returns a list of datasources for the specified dataset.
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         dataset_id:
             The Power Bi Dataset id. You can take it from PBI Service URL
@@ -155,18 +93,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/datasets/{}/datasources".format(dataset_id)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_dataset_to_dataflows_links_in_group(auth_token, workspace_id):
+    def get_dataset_to_dataflows_links_in_group(self, workspace_id):
         """Returns a list of upstream dataflows for datasets from the specified workspace.
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         workspace_id:
             The Power Bi workspace id. You can take it from PBI Service URL
@@ -177,18 +115,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/groups/{}/datasets/upstreamDataflows".format(workspace_id)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
         
-    def get_reports(auth_token):
+    def get_reports(self):
         """Returns a list of reports for the organization.
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         ### Returns
         ----
@@ -197,18 +135,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/reports"
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_reports_in_group(auth_token, workspace_id):
+    def get_reports_in_group(self, workspace_id):
         """Returns a list of reports from the specified workspace.
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         workspace_id:
             The Power Bi workspace id. You can take it from PBI Service URL
@@ -219,18 +157,18 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/groups/{}/reports".format(workspace_id)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
         except requests.exceptions.RequestException as e:
             print(e)
             
-    def get_reports_users(auth_token, report_id):
+    def get_reports_users(self, report_id):
         """Returns a list of users that have access to the specified report (Preview).
         ### Parameters
         ----
-        auth_token: str
+        self.token: str
             The Bearer Token to authenticate with Power Bi Rest API requests.
         dataset_id:
             The Power Bi Report id. You can take it from PBI Service URL
@@ -241,7 +179,7 @@ class admin():
         """
         try:
             url = "https://api.powerbi.com/v1.0/myorg/admin/reports/{}/users".format(report_id)
-            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(auth_token)})
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
             return response
         except requests.exceptions.HTTPError as ex:
             print(ex)
