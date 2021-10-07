0503eecaddc5f0414fb1a2504947d6a3b946488e
Ignacio Barrau <ibarrau@users.noreply.github.com>
2021-10-07 20:37:20 -0300

    Adding capacity, dataflows and dashboards
    Adding dashboards and tiles.
    Dataflows and datasources.
    Capacities

--- simplepbi/admin/__init__.py
+++ simplepbi/admin/__init__.py
@@ -322,6 +322,322 @@ class Admin():
         except requests.exceptions.RequestException as e:
             print(e)
 
+    def get_dashboards(self, expand=None filter=None, skip=None, top=None):
+        """Returns a list of dashboards for the organization.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        expand: string
+            Expands related entities inline, receives a comma-separated list of data types. Supported: users, reports, dashboards, datasets, dataflows, workbooks
+        filter: string
+            Filters the results based on a boolean condition
+        skip: int 
+            Skips the first n results. Use with top to fetch results beyond the first 5000.
+        top: int
+            Returns only the first n results. This parameter is mandatory and must be in the range of 1-5000.
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the dashboards in the tenant.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dashboards?"
+            if expand != None:
+                url = url + "$expand={}".format(expand)
+            if filter != None:
+                url = url + "&$filter={}".format(filter)
+            if top != None:
+                url = url + "&$top={}".format(top)
+            if skip != None:
+                url = url + "&$skip={}".format(skip)  
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_dashboards_in_group(self, workspace_id, filter=None, skip=None, top=None):
+        """Returns a list of dashboards from the specified workspace.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        workspace_id:
+            The Power Bi workspace id. You can take it from PBI Service URL
+        filter: string
+            Filters the results based on a boolean condition
+        skip: int 
+            Skips the first n results. Use with top to fetch results beyond the first 5000.
+        top: int
+            Returns only the first n results. This parameter is mandatory and must be in the range of 1-5000.
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the dashboards in the workspace.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/groups/{}/dashboards?".format(workspace_id)
+            if filter != None:
+                url = url + "$filter={}".format(filter)
+            if top != None:
+                url = url + "&$top={}".format(top)
+            if skip != None:
+                url = url + "&$skip={}".format(skip) 
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_dashboards_users(self, dashboard_id):
+        """Returns a list of users that have access to the specified dashboard (Preview).
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        dashboard_id:
+            The Power Bi dashboard id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the users in the dashboard.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dashboards/{}/users".format(dashboard_id)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+           
+    def get_tiles(self, dashboard_id):
+        """Returns a list of tiles within the specified dashboard.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        dashboard_id:
+            The Power Bi dashboard id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the tiles in the dashboard.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dashboards/{}/tiles".format(dashboard_id)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+    
+    def get_dataflows(self, filter=None, skip=None, top=None):
+        """Returns a list of dataflows for the organization.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        filter: string
+            Filters the results based on a boolean condition
+        skip: int 
+            Skips the first n results. Use with top to fetch results beyond the first 5000.
+        top: int
+            Returns only the first n results. This parameter is mandatory and must be in the range of 1-5000.
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the dataflows in the tenant.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dataflows?"
+            if filter != None:
+                url = url + "$filter={}".format(filter)
+            if top != None:
+                url = url + "&$top={}".format(top)
+            if skip != None:
+                url = url + "&$skip={}".format(skip)  
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_dataflows_in_group(self, workspace_id, filter=None, skip=None, top=None):
+        """Returns a list of dataflows from the specified workspace.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        workspace_id:
+            The Power Bi workspace id. You can take it from PBI Service URL
+        filter: string
+            Filters the results based on a boolean condition
+        skip: int 
+            Skips the first n results. Use with top to fetch results beyond the first 5000.
+        top: int
+            Returns only the first n results. This parameter is mandatory and must be in the range of 1-5000.
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the dataflows in the workspace.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/groups/{}/dataflows?".format(workspace_id)
+            if filter != None:
+                url = url + "$filter={}".format(filter)
+            if top != None:
+                url = url + "&$top={}".format(top)
+            if skip != None:
+                url = url + "&$skip={}".format(skip) 
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_dataflows_users(self, dataflow_id):
+        """Returns a list of users that have access to the specified dataflow (Preview).
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        dataflow_id:
+            The Power Bi dataflow id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the users in the dataflow.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dataflows/{}/users".format(dataflow_id)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+        
+    def get_dataflow_datasources(self, dataflow_id):
+        """Returns a list of datasources for the specified dataflow.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        dataflow_id:
+            The Power Bi Dataflow id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the datasources in the dataflow.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/dataflows/{}/datasources".format(dataflow_id)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_apps(self, top=None):
+        """Returns a list of apps for the organization.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        top: int
+            Returns only the first n results. This parameter is mandatory and must be in the range of 1-5000.
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the apps in the tenant.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/apps?"
+            if top != None:
+                url = url + "$top={}".format(top)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_apps_users(self, app_id):
+        """Returns a list of users that have access to the specified app (Preview).
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        app_id:
+            The Power Bi app id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the users in the app.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/apps/{}/users".format(app_id)
+            response = requests.get(url, headers={'Content-Type': 'application/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_capacities(self, expand=None):
+        """Returns a list of capacities for the organization.
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        expand: int
+            Expands related entities inline
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the capacities in the tenant.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/capacities?"
+            if expand != None:
+                url = url + "$expand={}".format(expand)
+            response = requests.get(url, headers={'Content-Type': 'capacitielication/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_capacities_users(self, capacity_id):
+        """Returns a list of users that have access to the specified capacitie (Preview).
+        ### Parameters
+        ----
+        self.token: str
+            The Bearer Token to authenticate with Power Bi Rest API requests.
+        capacity_id:
+            The Power Bi capacitie id. You can take it from PBI Service URL
+        ### Returns
+        ----
+        Dict:
+            A dictionary containing all the users in the capacity.
+        """
+        try:
+            url = "https://api.powerbi.com/v1.0/myorg/admin/capacities/{}/users".format(capacity_id)
+            response = requests.get(url, headers={'Content-Type': 'capacitielication/json', "Authorization": "Bearer {}".format(self.token)})
+            return response
+        except requests.exceptions.HTTPError as ex:
+            print(ex)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
     def get_activity_events():
         """Dummy description
         """
